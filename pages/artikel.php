7f2e058673be9bc38b01487c1426fce79f10d872
rizalvalry <rizal.muh.rzl@gmail.com>
2022-01-24 10:18:23 +0700

    fix

--- pages/artikel.php
+++ pages/artikel.php
@@ -15,7 +15,7 @@
 <div class="breadcrumbs">
    <div class="container">
       <ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
-         <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Galeri</a></li>
+         <li><a href="index.php"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Berita & Tips</a></li>
       </ol>
    </div>
 </div>
